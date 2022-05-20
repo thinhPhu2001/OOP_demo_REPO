0bc38b1146b54ce687402c1e9a68039a1d3a3f7e
thinh <thinh@THINH>
2022-05-20 12:54:10 +0700

    Add project files.

---
+++ OOP_demo/Emp.cs
@@ -0,0 +1,41 @@
+﻿namespace Prn.Se1624;
+
+public class  Emp
+{
+    // define fields(data)
+    private int _id;
+    private string _name;
+    private DateOnly _dob;
+
+    // define  properties 
+    public float Salary { get; set; }
+    public string Level { get; set; }
+    public int Id { get => _id; set => _id = value; }
+    public int Name { get => _name; set => _name = value; }
+    public DateOnly Dob { get => _dob; set => _dob = value; }
+
+    //constructrors
+    public Emp()
+    {
+
+    }
+
+    public Emp(float salary, string level, int id, int name, DateOnly dob)
+    {
+        Salary = salary;
+        Level = level;
+        Id = id;
+        Name = name;
+        Dob = dob;
+    }
+    // manual methods
+    public override string? ToString()
+    {
+        return GetString();
+    }
+    public GetString()
+    {
+        return $"[Id = {this._id}, Name = {Name}, Dob = {this._dob}],Level ={Level}, Salary = {Salary}";
+    }
+
+}
---
+++ OOP_demo/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Prn.Se1624;
+using static System.Console;
+
+public class Program
+{
+    public static void Main()
+    {
+        // 1.create obj
+        Emp e = new Emp();
+
+        e.Id = 1;
+
+        //2. 
+
+        WriteLine(e);
+        ReadLine();
+    }
+}
